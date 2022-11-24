5961a0dac0a05732a44cf663ac5a40e20e882314
Verubato <>
2022-11-25 08:35:16 +1100

    Replaced dropdowns with checkboxes to help with avoiding taint issues.

--- Options.lua
+++ Options.lua
@@ -7,146 +7,169 @@ local sortModeRole = "Role"
 local sortModeAlphabetical = "Alphabetical"
 local sortModeTop = "Top"
 local sortModeBottom = "Bottom"
+local verticalSpacing = -16
+local horizontalSpacing = 50
 
 function builder:BuiltTitle(panel)
     local title = panel:CreateFontString("lblTitle", "ARTWORK", "GameFontNormalLarge")
-    title:SetPoint("TOPLEFT", 16, -16)
+    title:SetPoint("TOPLEFT", 16, verticalSpacing)
     title:SetText("Frame Sort")
 
     local description = panel:CreateFontString("lblDescription", "ARTWORK", "GameFontWhite")
-    description:SetPoint("TOPLEFT", title, "BOTTOMLEFT", 0, -16)
+    description:SetPoint("TOPLEFT", title, "BOTTOMLEFT", 0, verticalSpacing)
     description:SetText("Sorts party/raid frames.")
+
+    local note = panel:CreateFontString("lblNote", "ARTWORK", "GameFontWhite")
+    note:SetPoint("TOPLEFT", description, "BOTTOMLEFT", 0, verticalSpacing)
+    note:SetText("Note: the weird usage of checkboxes here instead of dropdowns are to help avoiding taint issues.")
 end
 
-function builder.BuildPartySortEnabled(panel)
-    local cb = CreateFrame("CheckButton", "chkEnablePartySort", panel, "UICheckButtonTemplate")
-    cb:SetPoint("TOPLEFT", lblDescription, "BOTTOMLEFT", 0, -16)
-    cb.Text:SetText("Sort Party Frames?")
-    cb:SetChecked(addon.Options.PartySortEnabled)
-    cb:HookScript("OnClick", function(_, _, _)
-        addon.Options.PartySortEnabled = cb:GetChecked()
+function builder.BuildSortEnabled(panel)
+    local party = CreateFrame("CheckButton", "chkEnablePartySort", panel, "UICheckButtonTemplate")
+    party:SetPoint("TOPLEFT", lblNote, "BOTTOMLEFT", 0, verticalSpacing)
+    party.Text:SetText("Sort Party Frames?")
+    party:SetChecked(addon.Options.PartySortEnabled)
+    party:HookScript("OnClick", function(_, _, _)
+        addon.Options.PartySortEnabled = party:GetChecked()
     end)
-end
 
-function builder.BuildRaidSortEnabled(panel)
-    local cb = CreateFrame("CheckButton", "chkEnableRaidSort", panel, "UICheckButtonTemplate")
-    cb:SetPoint("LEFT", chkEnablePartySort, "RIGHT", 120, 0)
-    cb.Text:SetText("Sort Raid Frames?")
-    cb:SetChecked(addon.Options.RaidSortEnabled)
-    cb:HookScript("OnClick", function(_, _, _)
-        addon.Options.RaidSortEnabled = cb:GetChecked()
+    local raid = CreateFrame("CheckButton", "chkEnableRaidSort", panel, "UICheckButtonTemplate")
+    raid:SetPoint("LEFT", party, "RIGHT", 120, 0)
+    raid.Text:SetText("Sort Raid Frames?")
+    raid:SetChecked(addon.Options.RaidSortEnabled)
+    raid:HookScript("OnClick", function(_, _, _)
+        addon.Options.RaidSortEnabled = raid:GetChecked()
     end)
 end
 
 function builder:BuildPlayerSortMode(panel)
-    local label = panel:CreateFontString("lblPartySort", "ARTWORK", "GameFontNormal")
-    label:SetPoint("TOPLEFT", chkEnablePartySort, "BOTTOMLEFT", 0, -16)
+    local label = panel:CreateFontString("lblPlayerSort", "ARTWORK", "GameFontNormal")
+    label:SetPoint("TOPLEFT", chkEnablePartySort, "BOTTOMLEFT", 0, verticalSpacing)
     label:SetText("Player sort mode: ")
 
-    local dropDown = CreateFrame("FRAME", "ddlPlayerSortMode", panel, "UIDropDownMenuTemplate")
-    dropDown:SetPoint("TOPLEFT", label, "BOTTOMLEFT", -18, -16)
-    UIDropDownMenu_SetText(dropDown, addon.Options.PlayerSortMode)
+    -- why use checkboxes instead of a uidropdown?
+    -- because the uidropdown has so many taint issues for years that still haven't been fixed
+    -- and seem to have become much worse in dragonflight
+    -- so while a dropdown would be better ui design, it's too buggy to use
+    local top = CreateFrame("CheckButton", "chkPlayerSortModeTop", panel, "UICheckButtonTemplate")
+    local bottom = CreateFrame("CheckButton", "chkPlayerSortModeBottom", panel, "UICheckButtonTemplate")
 
-    function SetPlayerSortMode(_, sortMode)
-        addon.Options.PlayerSortMode = sortMode
-        UIDropDownMenu_SetText(dropDown, sortMode)
+    top:SetPoint("TOPLEFT", label, "BOTTOMLEFT", 0, verticalSpacing)
+    top.Text:SetText(sortModeTop)
+    top:SetChecked(addon.Options.PlayerSortMode == sortModeTop)
+    top:HookScript("OnClick", function(_, _, _)
+        if not top:GetChecked() then return end
 
+        addon.Options.PlayerSortMode = sortModeTop
+        bottom:SetChecked(false)
         addon:ConfigChanged()
-    end
+    end)
 
-    UIDropDownMenu_Initialize(dropDown, function()
-        UIDropDownMenu_AddButton({
-            text = sortModeTop,
-            func = SetPlayerSortMode,
-            arg1 = sortModeTop,
-            checked = addon.Options.PlayerSortMode == sortModeTop
-        })
-        UIDropDownMenu_AddButton({
-            text = sortModeBottom,
-            func = SetPlayerSortMode,
-            arg1 = sortModeBottom,
-            checked = addon.Options.PlayerSortMode == sortModeBottom
-        })
+    bottom:SetPoint("LEFT", top, "RIGHT", horizontalSpacing, 0)
+    bottom.Text:SetText(sortModeBottom)
+    bottom:SetChecked(addon.Options.PlayerSortMode == sortModeBottom)
+    bottom:HookScript("OnClick", function(_, _, _)
+        if not bottom:GetChecked() then return end
+
+        addon.Options.PlayerSortMode = sortModeBottom
+        top:SetChecked(false)
+        addon:ConfigChanged()
     end)
 end
 
 function builder:BuildPartySortMode(panel)
     local label = panel:CreateFontString("lblPartySort", "ARTWORK", "GameFontNormal")
-    label:SetPoint("TOPLEFT", ddlPlayerSortMode, "BOTTOMLEFT", 18, -16)
+    label:SetPoint("TOPLEFT", chkPlayerSortModeTop, "BOTTOMLEFT", 0, verticalSpacing)
     label:SetText("Party sort mode: ")
 
-    local dropDown = CreateFrame("FRAME", "ddlPartySortMode", panel, "UIDropDownMenuTemplate")
-    dropDown:SetPoint("TOPLEFT", label, "BOTTOMLEFT", -18, -16)
-    UIDropDownMenu_SetText(dropDown, addon.Options.PartySortMode)
+    local group = CreateFrame("CheckButton", "chkPartySortModeGroup", panel, "UICheckButtonTemplate")
+    local role = CreateFrame("CheckButton", "chkPartySortModeRole", panel, "UICheckButtonTemplate")
+    local alpha = CreateFrame("CheckButton", "chkPartySortModeAlpha", panel, "UICheckButtonTemplate")
 
-    function SetPartySortMode(_, sortMode)
-        addon.Options.PartySortMode = sortMode
-        UIDropDownMenu_SetText(dropDown, sortMode)
+    group:SetPoint("TOPLEFT", label, "BOTTOMLEFT", 0, verticalSpacing)
+    group.Text:SetText(sortModeGroup)
+    group:SetChecked(addon.Options.PartySortMode == sortModeGroup)
+    group:HookScript("OnClick", function(_, _, _)
+        if not group:GetChecked() then return end
 
+        addon.Options.PartySortMode = sortModeGroup
+        role:SetChecked(false)
+        alpha:SetChecked(false)
         addon:ConfigChanged()
-    end
+    end)
 
-    UIDropDownMenu_Initialize(dropDown, function()
-        UIDropDownMenu_AddButton({
-            text = sortModeGroup,
-            func = SetPartySortMode,
-            arg1 = sortModeGroup,
-            checked = addon.Options.PartySortMode == sortModeGroup
-        })
-        UIDropDownMenu_AddButton({
-            text = sortModeRole,
-            func = SetPartySortMode,
-            arg1 = sortModeRole,
-            checked = addon.Options.PartySortMode == sortModeRole
-        })
-        UIDropDownMenu_AddButton({
-            text = sortModeAlphabetical,
-            func = SetPartySortMode,
-            arg1 = sortModeAlphabetical,
-            checked = addon.Options.PartySortMode == sortModeAlphabetical
-        })
+    role:SetPoint("LEFT", group, "RIGHT", horizontalSpacing, 0)
+    role.Text:SetText(sortModeRole)
+    role:SetChecked(addon.Options.PartySortMode == sortModeRole)
+    role:HookScript("OnClick", function(_, _, _)
+        if not role:GetChecked() then return end
+
+        addon.Options.PartySortMode = sortModeRole
+        group:SetChecked(false)
+        alpha:SetChecked(false)
+        addon:ConfigChanged()
+    end)
+
+    alpha:SetPoint("LEFT", role, "RIGHT", horizontalSpacing, 0)
+    alpha.Text:SetText(sortModeAlphabetical)
+    alpha:SetChecked(addon.Options.PartySortMode == sortModeAlphabetical)
+    alpha:HookScript("OnClick", function(_, _, _)
+        if not alpha:GetChecked() then return end
+
+        addon.Options.PartySortMode = sortModeAlphabetical
+        group:SetChecked(false)
+        role:SetChecked(false)
+        addon:ConfigChanged()
     end)
 end
 
 function builder:BuildRaidSortMode(panel)
     local label = panel:CreateFontString("lblRaidSort", "ARTWORK", "GameFontNormal")
-    label:SetPoint("TOPLEFT", ddlPartySortMode, "BOTTOMLEFT", 18, -16)
+    label:SetPoint("TOPLEFT", chkPartySortModeGroup, "BOTTOMLEFT", 0, verticalSpacing)
     label:SetText("Raid sort mode: ")
 
-    local dropDown = CreateFrame("FRAME", "ddlRaidSortMode", panel, "UIDropDownMenuTemplate")
-    dropDown:SetPoint("TOPLEFT", label, "BOTTOMLEFT", -18, -16)
-    UIDropDownMenu_SetText(dropDown, addon.Options.RaidSortMode)
+    local group = CreateFrame("CheckButton", "chkRaidSortModeGroup", panel, "UICheckButtonTemplate")
+    local role = CreateFrame("CheckButton", "chkRaidSortModeRole", panel, "UICheckButtonTemplate")
+    local alpha = CreateFrame("CheckButton", "chkRaidSortModeAlpha", panel, "UICheckButtonTemplate")
 
-    function SetRaidSortMode(_, sortMode)
-        addon.Options.RaidSortMode = sortMode
-        UIDropDownMenu_SetText(dropDown, sortMode)
+    group:SetPoint("TOPLEFT", label, "BOTTOMLEFT", 0, verticalSpacing)
+    group.Text:SetText(sortModeGroup)
+    group:SetChecked(addon.Options.RaidSortMode == sortModeGroup)
+    group:HookScript("OnClick", function(_, _, _)
+        if not group:GetChecked() then return end
 
+        addon.Options.RaidSortMode = sortModeGroup
+        role:SetChecked(false)
+        alpha:SetChecked(false)
         addon:ConfigChanged()
-    end
+    end)
 
-    UIDropDownMenu_Initialize(dropDown, function()
-        UIDropDownMenu_AddButton({
-            text = sortModeGroup,
-            func = SetRaidSortMode,
-            arg1 = sortModeGroup,
-            checked = addon.Options.RaidSortMode == sortModeGroup
-        })
-        UIDropDownMenu_AddButton({
-            text = sortModeRole,
-            func = SetRaidSortMode,
-            arg1 = sortModeRole,
-            checked = addon.Options.RaidSortMode == sortModeRole
-        })
-        UIDropDownMenu_AddButton({
-            text = sortModeAlphabetical,
-            func = SetRaidSortMode,
-            arg1 = sortModeAlphabetical,
-            checked = addon.Options.RaidSortMode == sortModeAlphabetical
-        })
+    role:SetPoint("LEFT", group, "RIGHT", horizontalSpacing, 0)
+    role.Text:SetText(sortModeRole)
+    role:SetChecked(addon.Options.RaidSortMode == sortModeRole)
+    role:HookScript("OnClick", function(_, _, _)
+        if not role:GetChecked() then return end
+
+        addon.Options.RaidSortMode = sortModeRole
+        group:SetChecked(false)
+        alpha:SetChecked(false)
+        addon:ConfigChanged()
+    end)
+
+    alpha:SetPoint("LEFT", role, "RIGHT", horizontalSpacing, 0)
+    alpha.Text:SetText(sortModeAlphabetical)
+    alpha:SetChecked(addon.Options.RaidSortMode == sortModeAlphabetical)
+    alpha:HookScript("OnClick", function(_, _, _)
+        if not alpha:GetChecked() then return end
+
+        addon.Options.RaidSortMode = sortModeAlphabetical
+        group:SetChecked(false)
+        role:SetChecked(false)
+        addon:ConfigChanged()
     end)
 end
 
--- invoked when configuration changes which will then perform a re-sort
+-- invoked when configuration changes which will then perform a resort
 function addon:ConfigChanged()
     addon.NeedsSort = true
     addon:TrySort()
@@ -157,8 +180,7 @@ function addon:InitOptions()
     panel.name = addonName
 
     builder:BuiltTitle(panel)
-    builder.BuildPartySortEnabled(panel)
-    builder.BuildRaidSortEnabled(panel)
+    builder.BuildSortEnabled(panel)
     builder:BuildPlayerSortMode(panel)
     builder:BuildPartySortMode(panel)
     builder:BuildRaidSortMode(panel)
@@ -172,3 +194,4 @@ function addon:InitOptions()
         InterfaceOptionsFrame_OpenToCategory(addonName)
     end
 end
+
