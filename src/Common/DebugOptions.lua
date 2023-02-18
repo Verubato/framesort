1ae2b2ac55d4d91a96441642527bdcee7844fec2
Verubato <118912121+Verubato@users.noreply.github.com>
2023-02-19 02:24:37 +1100

    Moved options to child panels.

---
+++ src/Common/DebugOptions.lua
@@ -0,0 +1,25 @@
+local addonName, addon = ...
+local builder = addon.OptionsBuilder
+local verticalSpacing = -16
+
+---Adds the debug options panel.
+---@param parentPanel table the parent UI panel.
+function builder:BuildDebugOptions(parentPanel)
+    local panel = CreateFrame("Frame", addonName .. "Debug", parent)
+    panel.name = "Debug"
+    panel.parent = parentPanel.name
+
+    local enabled = CreateFrame("CheckButton", "chkDebugEnabled", panel, "UICheckButtonTemplate")
+    enabled:SetPoint("TOPLEFT", panel, 0, verticalSpacing)
+    enabled.Text:SetText("Debug mode")
+    enabled.Text:SetFontObject("GameFontNormalLarge")
+    enabled:SetChecked(addon.Options.DebugEnabled or false)
+    enabled:HookScript("OnClick", function() addon:SetOption("DebugEnabled", enabled:GetChecked()) end)
+
+    local description = panel:CreateFontString("lblDebugDescription", "ARTWORK", "GameFontWhite")
+    description:SetPoint("TOPLEFT", enabled, "BOTTOMLEFT", 4, verticalSpacing)
+    description:SetText("Logs messages to the chat panel which is useful for diagnosing bugs.")
+
+    InterfaceOptions_AddCategory(panel)
+end
+
