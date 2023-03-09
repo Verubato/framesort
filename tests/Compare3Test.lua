2f8c6e7df33f0b615df6bb6dc6ef5d599c6a84be
Verubato <118912121+Verubato@users.noreply.github.com>
2023-03-10 08:46:31 +1100

    Fixed failing unit tests.

--- tests/Compare3Test.lua
+++ tests/Compare3Test.lua
@@ -21,33 +21,33 @@ function M:setUp()
 end
 
 function M:test_sort_with_nonexistant_units()
-    assertEquals(addon:Compare("party3", "player", addon.SortMode.Top, addon.SortMode.Group), false)
-    assertEquals(addon:Compare("player", "party3", addon.SortMode.Top, addon.SortMode.Group), true)
+    assertEquals(addon:Compare("party3", "player", addon.PlayerSortMode.Top, addon.GroupSortMode.Group), false)
+    assertEquals(addon:Compare("player", "party3", addon.PlayerSortMode.Top, addon.GroupSortMode.Group), true)
 end
 
 function M:test_sort_player_top()
-    assertEquals(addon:Compare("player", "party1", addon.SortMode.Top, addon.SortMode.Group), true)
-    assertEquals(addon:Compare("player", "party2", addon.SortMode.Top, addon.SortMode.Group), true)
+    assertEquals(addon:Compare("player", "party1", addon.PlayerSortMode.Top, addon.GroupSortMode.Group), true)
+    assertEquals(addon:Compare("player", "party2", addon.PlayerSortMode.Top, addon.GroupSortMode.Group), true)
 
-    assertEquals(addon:Compare("party1", "player", addon.SortMode.Top, addon.SortMode.Group), false)
-    assertEquals(addon:Compare("party2", "player", addon.SortMode.Top, addon.SortMode.Group), false)
+    assertEquals(addon:Compare("party1", "player", addon.PlayerSortMode.Top, addon.GroupSortMode.Group), false)
+    assertEquals(addon:Compare("party2", "player", addon.PlayerSortMode.Top, addon.GroupSortMode.Group), false)
 end
 
 function M:test_sort_player_bottom()
-    assertEquals(addon:Compare("player", "party1", addon.SortMode.Bottom, addon.SortMode.Group), false)
-    assertEquals(addon:Compare("player", "party2", addon.SortMode.Bottom, addon.SortMode.Group), false)
+    assertEquals(addon:Compare("player", "party1", addon.PlayerSortMode.Bottom, addon.GroupSortMode.Group), false)
+    assertEquals(addon:Compare("player", "party2", addon.PlayerSortMode.Bottom, addon.GroupSortMode.Group), false)
 
-    assertEquals(addon:Compare("party1", "player", addon.SortMode.Bottom, addon.SortMode.Group), true)
-    assertEquals(addon:Compare("party2", "player", addon.SortMode.Bottom, addon.SortMode.Group), true)
+    assertEquals(addon:Compare("party1", "player", addon.PlayerSortMode.Bottom, addon.GroupSortMode.Group), true)
+    assertEquals(addon:Compare("party2", "player", addon.PlayerSortMode.Bottom, addon.GroupSortMode.Group), true)
 end
 
 function M:test_sort_player_middle()
     local presorted = { "player", "party1", "party2" }
-    assertEquals(addon:Compare("player", "party1", addon.SortMode.Middle, addon.SortMode.Group, presorted), false)
-    assertEquals(addon:Compare("player", "party2", addon.SortMode.Middle, addon.SortMode.Group, presorted), true)
+    assertEquals(addon:Compare("player", "party1", addon.PlayerSortMode.Middle, addon.GroupSortMode.Group, presorted), false)
+    assertEquals(addon:Compare("player", "party2", addon.PlayerSortMode.Middle, addon.GroupSortMode.Group, presorted), true)
 
-    assertEquals(addon:Compare("party1", "player", addon.SortMode.Middle, addon.SortMode.Group, presorted), true)
-    assertEquals(addon:Compare("party2", "player", addon.SortMode.Middle, addon.SortMode.Group, presorted), false)
+    assertEquals(addon:Compare("party1", "player", addon.PlayerSortMode.Middle, addon.GroupSortMode.Group, presorted), true)
+    assertEquals(addon:Compare("party2", "player", addon.PlayerSortMode.Middle, addon.GroupSortMode.Group, presorted), false)
 end
 
 return M
