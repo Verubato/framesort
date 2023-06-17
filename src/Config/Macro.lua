83df725038b0e916cee89644d2dae93e4b53431f
Verubato <118912121+Verubato@users.noreply.github.com>
2023-06-17 20:05:13 +1000

    Macro examples are now copyable.

--- src/Config/Macro.lua
+++ src/Config/Macro.lua
@@ -33,48 +33,83 @@ function fsBuilder:BuildMacroOptions(parent)
     countLine:SetPoint("TOPLEFT", title, "BOTTOMLEFT", 0, -verticalSpacing)
     countLine:SetText("FrameSort has found 0 macros to manage.")
 
-    local lines = {
+    panel:HookScript("OnShow", function()
+        local count = CountMacros()
+        countLine:SetText("FrameSort has found " .. count .. " |4macro:macros; to manage.")
+    end)
+
+    local intro = {
         "FrameSort will dynamically update macros with the \"#FrameSort\" header.",
         "Below are some examples on how to use this.",
-        "",
-        "Example 1",
-        "",
-        "#showtooltip",
-        "#FrameSort Frame1",
-        "/cast [@none] Spell;",
-        "",
-        "Example 2",
-        "",
-        "#framesort frame2",
-        "/cast [@placeholder,help] Dispel; [@placeholder,harm] Purge;",
-        "",
-        "Example 3",
-        "",
-        "#FrameSort: frame1 frame2",
-        "/cast [@a] Spell; [mod:shift, @b] Spell;",
-        "",
-        "Example 4",
-        "",
-        "#framesort: frame1, frame2, frame3",
-        "/cast [@a] Spell; [mod:shift, @b] Spell; [mod:ctrl, @c] Spell;",
-        "",
-        "Notes:",
-        " - The \"@\" placeholder values can be anything, e.g. @none, or @placeholder, or @a.",
-        " - Order matters, e.g. \"#framesort frame2, frame1\" would replace the first '@' with frame2."
     }
 
     local anchor = countLine
-    for i, line in ipairs(lines) do
+    for i, line in ipairs(intro) do
         local description = panel:CreateFontString(nil, "ARTWORK", "GameFontWhite")
         description:SetPoint("TOPLEFT", anchor, "BOTTOMLEFT", 0, i == 1 and -verticalSpacing or -verticalSpacing / 2)
         description:SetText(line)
         anchor = description
     end
 
-    panel:HookScript("OnShow", function()
-        local count = CountMacros()
-        countLine:SetText("FrameSort has found " .. count .. " |4macro:macros; to manage.")
-    end)
+    local examples = {
+        [[#showtooltip
+#FrameSort Frame1
+/cast [@none] Spell;]],
+        [[#framesort frame2
+/cast [@placeholder,help] Dispel; [@placeholder,harm] Purge;]],
+        [[#FrameSort: frame1 frame2
+/cast [@a] Spell; [mod:shift, @b] Spell;]],
+        [[#framesort: frame1, frame2, frame3
+/cast [@a] Spell; [mod:shift, @b] Spell; [mod:ctrl, @c] Spell;]]
+    }
+
+    local padding = 10
+    for i, example in ipairs(examples) do
+        local header = panel:CreateFontString(nil, "ARTWORK", "GameFontWhite")
+        header:SetPoint("TOPLEFT", anchor, "BOTTOMLEFT", 0, -verticalSpacing)
+        header:SetText(string.format("Example %d", i))
+
+        local box = CreateFrame("EditBox", nil, panel)
+        box:SetPoint("TOPLEFT", header, "BOTTOMLEFT", 0, -verticalSpacing)
+        box:SetSize(400, 1)
+        box:SetFontObject("GameFontWhite")
+        box:SetAutoFocus(false)
+        box:SetMultiLine(true)
+        box:SetText(example)
+        box:SetCursorPosition(0)
+
+        -- undo any user changes
+        box:SetScript("OnTextChanged", function(_, userInput)
+            if not userInput then return end
+
+            box:SetText(example)
+        end)
+
+        box:SetScript("OnEscapePressed", function() box:ClearFocus() end)
+        box:SetTextInsets(padding, padding, padding, padding)
+
+        local bg = CreateFrame("Frame", nil, panel, "BackdropTemplate")
+        bg:SetBackdrop({
+            edgeFile = "Interface\\Glues\\Common\\TextPanel-Border",
+            edgeSize = 16,
+        })
+        bg:SetAllPoints(box)
+
+        anchor = box
+    end
+
+    local notes = {
+        "Notes:",
+        " - The \"@\" placeholder values can be anything, e.g. @none, or @placeholder, or @a.",
+        " - Order matters, e.g. \"#framesort frame2, frame1\" would replace the first '@' with frame2."
+    }
+
+    for i, line in ipairs(notes) do
+        local description = panel:CreateFontString(nil, "ARTWORK", "GameFontWhite")
+        description:SetPoint("TOPLEFT", anchor, "BOTTOMLEFT", 0, i == 1 and -verticalSpacing or -verticalSpacing / 2)
+        description:SetText(line)
+        anchor = description
+    end
 
     InterfaceOptions_AddCategory(panel)
 end
