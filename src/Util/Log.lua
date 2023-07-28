8c81ddddb5ce3c5c3b56cf311cb5bc5942e62020
Verubato <>
2023-07-28 19:05:53 +1000

    Removed the storing log entries and just print instead.

--- src/Util/Log.lua
+++ src/Util/Log.lua
@@ -2,30 +2,15 @@ local _, addon = ...
 local logLevelDebug = "Debug"
 local logLevelWarning = "Warning"
 local logLevelError = "Error"
-local printOutput = false
-local entries = {}
 local M = {}
 addon.Log = M
 
 local function Write(msg, level)
-    if printOutput then
-        print(string.format("FrameSort: %s - %s", level, msg))
-    end
-
     if not addon.Options.Logging or not addon.Options.Logging.Enabled then
         return
     end
 
-    entries[#entries + 1] = {
-        Message = msg,
-        Level = level,
-        Timestamp = date("%Y-%m-%d %H:%M:%S"),
-    }
-end
-
-function addon:InitLogging()
-    -- reset the log on each run
-    FrameSortDB.Log = entries
+    print(string.format("FrameSort: %s - %s", level, msg))
 end
 
 ---Logs a debug message.
