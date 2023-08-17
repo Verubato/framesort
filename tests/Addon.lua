a46733e999662acdeae82c026730c66a43f73d0b
Verubato <118912121+Verubato@users.noreply.github.com>
2023-08-17 14:26:05 +1000

    Some refactoring.

---
+++ tests/Addon.lua
@@ -0,0 +1,18 @@
+local helper = require("Helper")
+local wow = require("Mock\\WoW")
+local provider = require("Mock\\FrameProvider")
+local addon = { WoW = wow }
+
+local dependencies = helper:DependenciesFromXml()
+helper:LoadDependencies(addon, dependencies)
+
+addon.Frame.Providers.Test = provider
+addon.Frame.Providers.All[#addon.Frame.Providers.All + 1] = provider
+
+function addon:Reset()
+    wow:Reset()
+    provider.Frames = {}
+    provider.Callbacks = {}
+end
+
+return addon
