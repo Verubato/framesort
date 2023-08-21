ebd69ea8bcf0eaa3ef6fc4c2945c743804c8a5bb
Verubato <>
2023-08-21 16:14:48 +1000

    Added a component test for the Api.

--- src/Configuration/SortMode.lua
+++ src/Configuration/SortMode.lua
@@ -1,17 +1,28 @@
 ---@type string, Addon
 local _, addon = ...
 
----@class GroupSortMode
+---@class GroupSortModeEnum
 addon.Configuration.GroupSortMode = {
     Group = "Group",
     Role = "Role",
     Alphabetical = "Alphabetical",
 }
 
----@class PlayerSortMode
+---@class PlayerSortModeEnum
 addon.Configuration.PlayerSortMode = {
     Top = "Top",
     Middle = "Middle",
     Bottom = "Bottom",
     Hidden = "Hidden",
 }
+
+---@alias PlayerSortMode
+---| "Top"
+---| "Middle"
+---| "Bottom"
+---| "Hidden"
+
+---@alias GroupSortMode
+---| "Group"
+---| "Role"
+---| "Alphabetical"
