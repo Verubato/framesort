d3b672a7513a184fa3b8da1dbbe99f07319e9408
Verubato <>
2023-09-07 20:10:48 +1000

    Wording update.

--- src/Configuration/Announcement.lua
+++ src/Configuration/Announcement.lua
@@ -13,15 +13,30 @@ function M:Build(panel)
 
     local important = panel:CreateFontString(nil, "ARTWORK", "GameFontRedLarge")
     important:SetPoint("TOPLEFT", title, "BOTTOMLEFT", 0, -verticalSpacing)
-    important:SetText("Blizzard patch 10.1.7 has broken FrameSort.")
+    important:SetText("WoW patch 10.1.7 has broken FrameSort.")
 
-    local more = panel:CreateFontString(nil, "ARTWORK", "GameFontWhite")
-    more:SetPoint("TOPLEFT", important, "BOTTOMLEFT", 0, -verticalSpacing)
-    more:SetText("More info in the below Reddit post.")
+    local lines = {
+        "Blizzard have restricted the API method that FrameSort uses for sorting and spacing frames.",
+        "The API method can no longer be used during combat.",
+        "",
+        "This is a problem because sorting is lost when frames are refreshed mid-combat which happens",
+        "on events such as pet summons/dismisses, mind control, entering/exiting a vehicle,",
+        "killing a boss, and members joining/leaving the group.",
+        "",
+        "Further discussion on Reddit:",
+    }
+
+    local anchor = important
+    for i, text in ipairs(lines) do
+        local line = panel:CreateFontString(nil, "ARTWORK", "GameFontWhite")
+        line:SetPoint("TOPLEFT", anchor, "BOTTOMLEFT", 0, i == 1 and -verticalSpacing or -verticalSpacing / 2)
+        line:SetText(text)
+        anchor = line
+    end
 
     local redditLink = "https://www.reddit.com/r/worldofpvp/comments/16bjeli/psa_1017_has_broken_framesort/"
     local box = wow.CreateFrame("EditBox", nil, panel)
-    box:SetPoint("TOPLEFT", more, "BOTTOMLEFT", 0, -verticalSpacing)
+    box:SetPoint("TOPLEFT", anchor, "BOTTOMLEFT", 0, -verticalSpacing)
     box:SetSize(600, 1)
     box:SetFontObject("GameFontWhite")
     box:SetAutoFocus(false)
@@ -52,11 +67,15 @@ function M:Build(panel)
     })
     bg:SetAllPoints(box)
 
+    local workaround = panel:CreateFontString(nil, "ARTWORK", "GameFontNormalLarge")
+    workaround:SetPoint("TOPLEFT", box, "BOTTOMLEFT", 0, -verticalSpacing)
+    workaround:SetText("Workaround")
+
     local suggestion = panel:CreateFontString(nil, "ARTWORK", "GameFontWhite")
-    suggestion:SetPoint("TOPLEFT", box, "BOTTOMLEFT", 0, -verticalSpacing)
-    suggestion:SetText("As a workaround, you might like the Traditional sorting method instead of Taintless.")
+    suggestion:SetPoint("TOPLEFT", workaround, "BOTTOMLEFT", 0, -verticalSpacing)
+    suggestion:SetText("As a workaround, use the Traditional sorting method and /reload at the start of each arena match.")
 
     local fin = panel:CreateFontString(nil, "ARTWORK", "GameFontWhite")
     fin:SetPoint("TOPLEFT", suggestion, "BOTTOMLEFT", 0, -verticalSpacing)
-    fin:SetText("Hang in there bros while I try to find a solution.")
+    fin:SetText("Hang in there bros while I try to find a better solution.")
 end
