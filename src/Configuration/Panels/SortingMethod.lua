857cfb29a325ae6b3c6e0951525ba2873ec73d15
Verubato <118912121+Verubato@users.noreply.github.com>
2023-09-10 20:17:55 +1000

    Checkbox text change.

--- src/Configuration/Panels/SortingMethod.lua
+++ src/Configuration/Panels/SortingMethod.lua
@@ -36,12 +36,12 @@ function M:Build(parent)
     local secure = wow.CreateFrame("CheckButton", nil, panel, "UICheckButtonTemplate")
     -- not sure why, but checkbox left seems to be off by about 4 units by default
     secure:SetPoint("TOPLEFT", panel, verticalSpacing - 4, -verticalSpacing + 4)
-    secure.Text:SetText("Secure")
+    secure.Text:SetText("Secure *In Development*")
     secure.Text:SetFontObject("GameFontNormalLarge")
     secure:SetChecked(addon.DB.Options.SortingMethod == M.Secure)
 
     local secureLines = {
-        "Please help me test this and let me know how it goes.",
+        "Feel free to test this and let me know how it goes.",
     }
 
     local anchor = secure
