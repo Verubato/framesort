61e6ce87ce13a0867a3b6b7c2ad66c32f076b5d9
Verubato <>
2023-09-27 22:28:03 +1000

    Now running sort immediately on unit refresh instead of triggering an attribute change to be invoked in a future frame. Also some refactoring.

--- src/Modules/Sorting/Secure.lua
+++ src/Modules/Sorting/Secure.lua
@@ -8,68 +8,62 @@ local fsEnumerable = addon.Collections.Enumerable
 local M = {}
 addon.Modules.Sorting.Secure = M
 
-local secureManager = nil
-local groupHeader = nil
-local petHeader = nil
+local headers = nil
 
-local addFrameScript = [[
-    local provider = self:GetAttribute("provider")
-    local frames = FramesByProvider[provider]
-    local points = PointsByProvider[provider]
-    local frame = self:GetFrameRef("frame")
-    local destination = self:GetAttribute("frameType")
-    local point, relativeTo, relativePoint, offsetX, offsetY = frame:GetPoint()
-    local data = newtable()
-
-    data.point = point
-    data.relativeTo = relativeTo
-    data.relativePoint = relativePoint
-    data.offsetX = offsetX
-    data.offsetY = offsetY
-
-    tinsert(frames[destination], frame)
-    points[frame] = data
-]]
-
-local clearFramesScript = [[
-    local provider = self:GetAttribute("provider")
-    local frames = FramesByProvider[provider]
-
-    if not frames then
-        frames = newtable()
-        frames.Party = newtable()
-        frames.Raid = newtable()
-        frames.Arena = newtable()
-        frames.Groups = newtable()
-
-        FramesByProvider[provider] = frames
-    else
-        frames.Party = wipe(frames.Party)
-        frames.Raid = wipe(frames.Raid)
-        frames.Arena = wipe(frames.Arena)
-        frames.Groups = wipe(frames.Groups)
-    end
-
-    local points = PointsByProvider[provider]
-    PointsByProvider[provider] = points and wipe(points) or newtable()
-]]
-
-local function AddFrames(frames, type)
-    assert(secureManager ~= nil)
+local function AddFrames(header, provider, frames, type)
+    header:SetAttribute("FS-FrameType", type)
+    header:SetAttribute("FS-Provider", provider:Name())
 
     for _, frame in ipairs(frames) do
-        secureManager:SetFrameRef("frame", frame)
-        secureManager:SetAttribute("frameType", type)
-        secureManager:Execute(addFrameScript)
+        header:SetFrameRef("frame", frame)
+        header:Execute([[
+            local provider = self:GetAttribute("FS-Provider")
+            local frames = FramesByProvider[provider]
+            local points = PointsByProvider[provider]
+            local frame = self:GetFrameRef("frame")
+            local destination = self:GetAttribute("FS-FrameType")
+            local point, relativeTo, relativePoint, offsetX, offsetY = frame:GetPoint()
+            local data = newtable()
+
+            data.point = point
+            data.relativeTo = relativeTo
+            data.relativePoint = relativePoint
+            data.offsetX = offsetX
+            data.offsetY = offsetY
+
+            tinsert(frames[destination], frame)
+            points[frame] = data
+        ]])
     end
 end
 
 ---@param provider FrameProvider
-local function StoreFrames(provider)
-    assert(secureManager ~= nil)
+local function StoreFrames(header, provider)
+    header:SetAttribute("FS-Provider", provider:Name())
 
-    secureManager:SetAttribute("provider", provider:Name())
-    secureManager:Execute(clearFramesScript)
+    -- clear existing frames
+    header:Execute([[
+        local provider = self:GetAttribute("FS-Provider")
+        local frames = FramesByProvider[provider]
+
+        if not frames then
+            frames = newtable()
+            frames.Party = newtable()
+            frames.Raid = newtable()
+            frames.Arena = newtable()
+            frames.Groups = newtable()
+
+            FramesByProvider[provider] = frames
+        else
+            frames.Party = wipe(frames.Party)
+            frames.Raid = wipe(frames.Raid)
+            frames.Arena = wipe(frames.Arena)
+            frames.Groups = wipe(frames.Groups)
+        end
+
+        local points = PointsByProvider[provider]
+        PointsByProvider[provider] = points and wipe(points) or newtable()
+    ]])
 
     local friendlyEnabled, _, _, _ = fsCompare:FriendlySortMode()
     local enemyEnabled, _, _ = fsCompare:EnemySortMode()
@@ -99,17 +93,17 @@ local function StoreFrames(provider)
         arena = provider:EnemyArenaFrames()
     end
 
-    AddFrames(party, "Party")
-    AddFrames(raid, "Raid")
-    AddFrames(raidUngrouped, "Raid")
-    AddFrames(groups, "Groups")
-    AddFrames(arena, "Arena")
+    AddFrames(header, provider, party, "Party")
+    AddFrames(header, provider, raid, "Raid")
+    AddFrames(header, provider, raidUngrouped, "Raid")
+    AddFrames(header, provider, groups, "Groups")
+    AddFrames(header, provider, arena, "Arena")
 end
 
 function OnCombatStarting()
-    for _, provider in ipairs(fsProviders:Enabled()) do
-        StoreFrames(provider)
+    assert(headers ~= nil)
 
+    for _, provider in ipairs(fsProviders:Enabled()) do
         local containers = {
             provider:RaidContainer(),
             provider:PartyContainer(),
@@ -121,6 +115,10 @@ function OnCombatStarting()
         -- https://github.com/Stanzilla/WoWUIBugs/issues/480
         for _, container in pairs(containers) do
             container:SetProtected()
+        end
+
+        for _, header in ipairs(headers) do
+            StoreFrames(header, provider)
         end
     end
 end
@@ -151,29 +149,33 @@ local function InjectSecureHelpers(secureFrame)
     end
 end
 
-local function ConfigureHeader(header, manager)
+local function ConfigureHeader(header)
     InjectSecureHelpers(header)
-    header:SetFrameRef("Manager", manager)
 
-    -- secure snippet for refreshing unit ids on frames
-    -- which we can then use to invoke our sorting
+    function header:InvokeCallbacks()
+        fsSorting:InvokeCallbacks()
+    end
+
+    -- state tables
     header:Execute([=[
         Header = self
-        Manager = self:GetFrameRef("Manager")
-
-        RefreshUnitChange = [[
-            local unit = self:GetAttribute("unit")
-            local frame = self:GetAttribute("UnitFrame")
-
-            if frame then
-                frame:SetAttribute("unit", unit)
-            end
-
-            local manager = self:GetAttribute("Manager")
-            local run = manager:GetAttribute("state-framesort-run-toggle")
-            manager:SetAttribute("state-framesort-run-toggle", not run)
-        ]]
+        FramesByProvider = newtable()
+        PointsByProvider = newtable()
     ]=])
+
+    -- some utility functions
+    header:SetAttribute("FS-Round", [[
+        local number, decimalPlaces = ...
+
+        if number == nil then return nil end
+
+        local mult = 10 ^ (decimalPlaces or 0)
+        return math.floor(number * mult + 0.5) / mult
+    ]])
+
+    header:SetAttribute("FS-InCombat", [[
+        return SecureCmdOptionParse("[combat] true; false") == "true"
+    ]])
 
     -- show as much as possible
     header:SetAttribute("showRaid", true)
@@ -185,14 +187,105 @@ local function ConfigureHeader(header, manager)
     header:SetAttribute("template", "SecureHandlerAttributeTemplate")
 
     -- fired when a new unit button is created
-    header:SetAttribute("initialConfigFunction", [[
+    header:SetAttribute("initialConfigFunction", [=[
         -- self = the newly created unit button
         self:SetWidth(0)
         self:SetHeight(0)
-        self:SetAttribute("Header", Header)
-        self:SetAttribute("Manager", Manager)
+        self:SetAttribute("FS-Header", Header)
+
+        -- secure snippet for refreshing unit ids on frames
+        -- which we can then use to invoke our sorting
+        RefreshUnitChange = [[
+            local header = self:GetAttribute("FS-Header")
+            header:RunAttribute("FS-TrySort")
+        ]]
+
         self:SetAttribute("refreshUnitChange", RefreshUnitChange)
+    ]=])
+
+    -- TODO: delete these workaround triggers once it's all fixed
+    wow.RegisterAttributeDriver(header, "state-framesort-target", "[@target, exists] true; false")
+    wow.RegisterAttributeDriver(header, "state-framesort-modifier", "[mod] true; false")
+
+    for i = 1, wow.MAX_RAID_MEMBERS do
+        wow.RegisterAttributeDriver(header, "state-framesort-raid" .. i, string.format("[@raid%d, exists] true; false", i))
+        wow.RegisterAttributeDriver(header, "state-framesort-raidpet" .. i, string.format("[@raidpet%d, exists] true; false", i))
+    end
+
+    for i = 1, wow.MEMBERS_PER_RAID_GROUP - 1 do
+        wow.RegisterAttributeDriver(header, "state-framesort-party" .. i, string.format("[@party%d, exists] true; false", i))
+        wow.RegisterAttributeDriver(header, "state-framesort-partypet" .. i, string.format("[@partypet%d, exists] true; false", i))
+    end
+
+    header:SetAttribute("FS-TrySort", [[
+        if not self:RunAttribute("FS-InCombat") then
+            return false
+        end
+
+        local sorted = false
+        -- don't move frames if they are have minuscule position differences
+        -- it's just a rounding error and makes no visual impact
+        -- this helps preventing spam on our callbacks
+        local decimalSanity = 2
+
+        for provider, framesByType in pairs(FramesByProvider) do
+            for _, frames in pairs(framesByType) do
+                local framesToMove = newtable()
+
+                -- first determine which frames require moving and clear their points
+                for _, frame in ipairs(frames) do
+                    local to = PointsByProvider[provider][frame]
+                    if to then
+                        local point, relativeTo, relativePoint, offsetX, offsetY = frame:GetPoint()
+
+                        local offsetXRounded = self:RunAttribute("FS-Round", offsetX, decimalSanity)
+                        local offsetYRounded = self:RunAttribute("FS-Round", offsetY, decimalSanity)
+                        local toOffsetXRounded = self:RunAttribute("FS-Round", to.offsetX, decimalSanity)
+                        local toOffsetYRounded = self:RunAttribute("FS-Round", to.offsetY, decimalSanity)
+
+                        local different =
+                            point ~= to.point or
+                            relativeTo ~= to.relativeTo or
+                            relativePoint ~= to.relativePoint or
+                            offsetXRounded ~= toOffsetXRounded or
+                            offsetYRounded ~= toOffsetYRounded
+
+                        if different then
+                            framesToMove[#framesToMove + 1] = frame
+                            frame:ClearAllPoints()
+                        end
+                    end
+                end
+
+                -- now move them after all points have been cleared
+                -- to avoid any circular dependency issues
+                for _, frame in ipairs(framesToMove) do
+                    local to = PointsByProvider[provider][frame]
+
+                    frame:SetPoint(to.point, to.relativeTo, to.relativePoint, to.offsetX, to.offsetY)
+                end
+
+                sorted = sorted or #framesToMove > 0
+            end
+        end
+
+        if sorted then
+            -- notify unsecure code to invoke callbacks
+            self:CallMethod("InvokeCallbacks")
+        end
+
+        return sorted
     ]])
+
+    header:WrapScript(
+        header,
+        "OnAttributeChanged",
+        [[
+            if not strmatch(name, "framesort") then return end
+
+            self:RunAttribute("FS-TrySort")
+        ]]
+    )
 
     -- must be shown for it to work
     header:SetPoint("TOPLEFT", wow.UIParent, "TOPLEFT")
@@ -200,125 +293,17 @@ local function ConfigureHeader(header, manager)
 end
 
 function M:Init()
-    secureManager = wow.CreateFrame("Frame", nil, wow.UIParent, "SecureHandlerStateTemplate")
-    InjectSecureHelpers(secureManager)
+    local combatEndFrame = wow.CreateFrame("Frame")
+    combatEndFrame:HookScript("OnEvent", OnEvent)
+    combatEndFrame:RegisterEvent(wow.Events.PLAYER_REGEN_DISABLED)
 
-    secureManager:HookScript("OnEvent", OnEvent)
-    secureManager:RegisterEvent(wow.Events.PLAYER_REGEN_DISABLED)
+    local groupHeader = wow.CreateFrame("Frame", "FrameSortGroupHeader", wow.UIParent, "SecureGroupHeaderTemplate")
+    local petHeader = wow.CreateFrame("Frame", "FrameSortPetGroupHeader", wow.UIParent, "SecureGroupPetHeaderTemplate")
 
-    function secureManager:InvokeCallbacks()
-        fsSorting:InvokeCallbacks()
-    end
+    headers = { groupHeader, petHeader }
 
-    -- tables to store frames that we manage
-    secureManager:Execute([[
-        FramesByProvider = newtable()
-        PointsByProvider = newtable()
-    ]])
-
-    -- some utility functions
-    secureManager:Execute([=[
-        Round = [[
-            local number, decimalPlaces = ...
-
-            if number == nil then return nil end
-
-            local mult = 10 ^ (decimalPlaces or 0)
-            return math.floor(number * mult + 0.5) / mult
-        ]]
-
-        InCombat = [[
-            return SecureCmdOptionParse("[combat] true; false") == "true"
-        ]]
-    ]=])
-
-    -- main function to restore frame positions
-    -- TODO: see if possible to perform full sorting/spacing instead of just retaining positions
-    secureManager:Execute([=[
-        RestoreFrames = [[
-            local sorted = false
-            -- don't move frames if they are have minuscule position differences
-            -- it's just a rounding error and makes no visual impact
-            -- this helps preventing spam on our callbacks
-            local decimalSanity = 2
-
-            for provider, framesByType in pairs(FramesByProvider) do
-                for _, frames in pairs(framesByType) do
-                    local framesToMove = newtable()
-
-                    -- first determine which frames require moving and clear their points
-                    for _, frame in ipairs(frames) do
-                        local to = PointsByProvider[provider][frame]
-                        if to then
-                            local point, relativeTo, relativePoint, offsetX, offsetY = frame:GetPoint()
-
-                            local offsetXRounded = self:Run(Round, offsetX, decimalSanity)
-                            local offsetYRounded = self:Run(Round, offsetY, decimalSanity)
-                            local toOffsetXRounded = self:Run(Round, to.offsetX, decimalSanity)
-                            local toOffsetYRounded = self:Run(Round, to.offsetY, decimalSanity)
-
-                            local different =
-                                point ~= to.point or
-                                relativeTo ~= to.relativeTo or
-                                relativePoint ~= to.relativePoint or
-                                offsetXRounded ~= toOffsetXRounded or
-                                offsetYRounded ~= toOffsetYRounded
-
-                            if different then
-                                framesToMove[#framesToMove + 1] = frame
-                                frame:ClearAllPoints()
-                            end
-                        end
-                    end
-
-                    -- now move them after all points have been cleared
-                    -- to avoid any circular dependency issues
-                    for _, frame in ipairs(framesToMove) do
-                        local to = PointsByProvider[provider][frame]
-
-                        frame:SetPoint(to.point, to.relativeTo, to.relativePoint, to.offsetX, to.offsetY)
-                    end
-
-                    sorted = sorted or #framesToMove > 0
-                end
-            end
-
-            if sorted then
-                -- notify unsecure code to invoke callbacks
-                self:CallMethod("InvokeCallbacks")
-            end
-        ]]
-    ]=])
-
-    -- triggers
-    secureManager:WrapScript(
-        secureManager,
-        "OnAttributeChanged",
-        [[
-            if not strmatch(name, "framesort") then return end
-            if not self:Run(InCombat) then return end
-
-            self:Run(RestoreFrames)
-        ]]
-    )
-
-    groupHeader = wow.CreateFrame("Frame", nil, nil, "SecureGroupHeaderTemplate")
-    ConfigureHeader(groupHeader, secureManager)
-
-    petHeader = wow.CreateFrame("Frame", nil, nil, "SecureGroupPetHeaderTemplate")
-    ConfigureHeader(petHeader, secureManager)
-
-    wow.RegisterAttributeDriver(secureManager, "state-framesort-target", "[@target, exists] true; false")
-    wow.RegisterAttributeDriver(secureManager, "state-framesort-modifier", "[mod] true; false")
-
-    for i = 1, wow.MAX_RAID_MEMBERS do
-        wow.RegisterAttributeDriver(secureManager, "state-framesort-raid" .. i, string.format("[@raid%d, exists] true; false", i))
-        wow.RegisterAttributeDriver(secureManager, "state-framesort-raidpet" .. i, string.format("[@raidpet%d, exists] true; false", i))
-    end
-
-    for i = 1, wow.MEMBERS_PER_RAID_GROUP - 1 do
-        wow.RegisterAttributeDriver(secureManager, "state-framesort-party" .. i, string.format("[@party%d, exists] true; false", i))
-        wow.RegisterAttributeDriver(secureManager, "state-framesort-partypet" .. i, string.format("[@partypet%d, exists] true; false", i))
+    for _, header in ipairs(headers) do
+        ConfigureHeader(header)
     end
 end
 
