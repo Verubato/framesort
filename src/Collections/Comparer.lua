5365c700cf7a5c2ec07a2a881b56d1feb2c2247b
Verubato <>
2023-10-18 18:56:59 +1100

    Updated some unit tests.

--- src/Collections/Comparer.lua
+++ src/Collections/Comparer.lua
@@ -114,82 +114,6 @@ local function CompareMiddle(token, sortedUnits)
     return index > mid
 end
 
----Returns a function that accepts two parameters of unit tokens and returns true if the left token should be ordered before the right.
----Sorting is based on the current instance and configured options.
----@param units string[]? the set of all unit tokens, only required if the player sort mode is "Middle"
----@return function sort
-function M:SortFunction(units)
-    local enabled, playerSortMode, groupSortMode, reverse = M:FriendlySortMode()
-
-    if not enabled then
-        return EmptyCompare
-    end
-
-    if playerSortMode ~= fsConfig.PlayerSortMode.Middle then
-        return function(x, y)
-            return M:Compare(x, y, playerSortMode, groupSortMode, reverse)
-        end
-    end
-
-    units = units or fsUnit:FriendlyUnits()
-
-    -- we need to pre-sort to determine where the middle actually is
-    -- making use of Enumerable:OrderBy() so we don't re-order the original array
-    units = fsEnumerable
-        :From(units)
-        :Where(function(x) return not wow.UnitIsUnit(x, "player") end)
-        :OrderBy(function(x, y)
-            return M:Compare(x, y, fsConfig.PlayerSortMode.Top, groupSortMode, reverse)
-        end)
-        :ToTable()
-
-    return function(x, y)
-        return M:Compare(x, y, playerSortMode, groupSortMode, reverse, units)
-    end
-end
-
----Returns a function that accepts two parameters of unit tokens and returns true if the left token should be ordered before the right.
----@return function sort
-function M:EnemySortFunction()
-    local enabled, groupSortMode, reverse = M:EnemySortMode()
-
-    if not enabled then
-        return EmptyCompare
-    end
-
-    return function(x, y)
-        return M:EnemyCompare(x, y, groupSortMode, reverse)
-    end
-end
-
----Returns the sort mode from the configured options for the current instance.
----@return boolean enabled whether sorting is enabled.
----@return string? playerMode the player sort mode.
----@return string? groupMode the group sort mode.
----@return boolean? reverse whether the sorting is reversed.
-function M:FriendlySortMode()
-    local inInstance, instanceType = wow.IsInInstance()
-
-    if inInstance and instanceType == "arena" then
-        return addon.DB.Options.Arena.Enabled, addon.DB.Options.Arena.PlayerSortMode, addon.DB.Options.Arena.GroupSortMode, addon.DB.Options.Arena.Reverse
-    elseif inInstance and instanceType == "party" then
-        return addon.DB.Options.Dungeon.Enabled, addon.DB.Options.Dungeon.PlayerSortMode, addon.DB.Options.Dungeon.GroupSortMode, addon.DB.Options.Dungeon.Reverse
-    elseif inInstance and (instanceType == "raid" or instanceType == "pvp") then
-        return addon.DB.Options.Raid.Enabled, addon.DB.Options.Raid.PlayerSortMode, addon.DB.Options.Raid.GroupSortMode, addon.DB.Options.Raid.Reverse
-    end
-
-    -- default to world rules for all other instance types
-    return addon.DB.Options.World.Enabled, addon.DB.Options.World.PlayerSortMode, addon.DB.Options.World.GroupSortMode, addon.DB.Options.World.Reverse
-end
-
----Returns the sort mode from the configured options for the current instance.
----@return boolean enabled whether sorting is enabled.
----@return string? groupMode the group sort mode.
----@return boolean? reverse whether the sorting is reversed.
-function M:EnemySortMode()
-    return addon.DB.Options.EnemyArena.Enabled, addon.DB.Options.EnemyArena.GroupSortMode, addon.DB.Options.EnemyArena.Reverse
-end
-
 ---Returns true if the left token should be ordered before the right token.
 ---preSortedUnits is required if playerSortMode == Middle.
 ---@param leftToken string
@@ -199,7 +123,7 @@ end
 ---@param reverse boolean?
 ---@param preSortedUnits table?
 ---@return boolean
-function M:Compare(leftToken, rightToken, playerSortMode, groupSortMode, reverse, preSortedUnits)
+local function Compare(leftToken, rightToken, playerSortMode, groupSortMode, reverse, preSortedUnits)
     -- if not in a group, we might be in test mode
     if wow.IsInGroup() then
         if not wow.UnitExists(leftToken) then
@@ -219,7 +143,7 @@ function M:Compare(leftToken, rightToken, playerSortMode, groupSortMode, reverse
         local leftTokenParent = string.gsub(leftToken, "pet", "")
         local rightTokenParent = string.gsub(rightToken, "pet", "")
 
-        return M:Compare(leftTokenParent, rightTokenParent, playerSortMode, groupSortMode, reverse, preSortedUnits)
+        return Compare(leftTokenParent, rightTokenParent, playerSortMode, groupSortMode, reverse, preSortedUnits)
     end
 
     if playerSortMode and playerSortMode ~= "" then
@@ -269,7 +193,7 @@ end
 ---@param groupSortMode? string
 ---@param reverse boolean?
 ---@return boolean
-function M:EnemyCompare(leftToken, rightToken, groupSortMode, reverse)
+local function EnemyCompare(leftToken, rightToken, groupSortMode, reverse)
     if reverse then
         local tmp = leftToken
         leftToken = rightToken
@@ -287,6 +211,82 @@ function M:EnemyCompare(leftToken, rightToken, groupSortMode, reverse)
     end
 
     return leftToken < rightToken
+end
+
+---Returns a function that accepts two parameters of unit tokens and returns true if the left token should be ordered before the right.
+---Sorting is based on the current instance and configured options.
+---@param units string[]? the set of all unit tokens, only required if the player sort mode is "Middle"
+---@return function sort
+function M:SortFunction(units)
+    local enabled, playerSortMode, groupSortMode, reverse = M:FriendlySortMode()
+
+    if not enabled then
+        return EmptyCompare
+    end
+
+    if playerSortMode ~= fsConfig.PlayerSortMode.Middle then
+        return function(x, y)
+            return Compare(x, y, playerSortMode, groupSortMode, reverse)
+        end
+    end
+
+    units = units or fsUnit:FriendlyUnits()
+
+    -- we need to pre-sort to determine where the middle actually is
+    -- making use of Enumerable:OrderBy() so we don't re-order the original array
+    units = fsEnumerable
+        :From(units)
+        :Where(function(x) return not wow.UnitIsUnit(x, "player") end)
+        :OrderBy(function(x, y)
+            return Compare(x, y, fsConfig.PlayerSortMode.Top, groupSortMode, reverse)
+        end)
+        :ToTable()
+
+    return function(x, y)
+        return Compare(x, y, playerSortMode, groupSortMode, reverse, units)
+    end
+end
+
+---Returns a function that accepts two parameters of unit tokens and returns true if the left token should be ordered before the right.
+---@return function sort
+function M:EnemySortFunction()
+    local enabled, groupSortMode, reverse = M:EnemySortMode()
+
+    if not enabled then
+        return EmptyCompare
+    end
+
+    return function(x, y)
+        return EnemyCompare(x, y, groupSortMode, reverse)
+    end
+end
+
+---Returns the sort mode from the configured options for the current instance.
+---@return boolean enabled whether sorting is enabled.
+---@return string? playerMode the player sort mode.
+---@return string? groupMode the group sort mode.
+---@return boolean? reverse whether the sorting is reversed.
+function M:FriendlySortMode()
+    local inInstance, instanceType = wow.IsInInstance()
+
+    if inInstance and instanceType == "arena" then
+        return addon.DB.Options.Arena.Enabled, addon.DB.Options.Arena.PlayerSortMode, addon.DB.Options.Arena.GroupSortMode, addon.DB.Options.Arena.Reverse
+    elseif inInstance and instanceType == "party" then
+        return addon.DB.Options.Dungeon.Enabled, addon.DB.Options.Dungeon.PlayerSortMode, addon.DB.Options.Dungeon.GroupSortMode, addon.DB.Options.Dungeon.Reverse
+    elseif inInstance and (instanceType == "raid" or instanceType == "pvp") then
+        return addon.DB.Options.Raid.Enabled, addon.DB.Options.Raid.PlayerSortMode, addon.DB.Options.Raid.GroupSortMode, addon.DB.Options.Raid.Reverse
+    end
+
+    -- default to world rules for all other instance types
+    return addon.DB.Options.World.Enabled, addon.DB.Options.World.PlayerSortMode, addon.DB.Options.World.GroupSortMode, addon.DB.Options.World.Reverse
+end
+
+---Returns the sort mode from the configured options for the current instance.
+---@return boolean enabled whether sorting is enabled.
+---@return string? groupMode the group sort mode.
+---@return boolean? reverse whether the sorting is reversed.
+function M:EnemySortMode()
+    return addon.DB.Options.EnemyArena.Enabled, addon.DB.Options.EnemyArena.GroupSortMode, addon.DB.Options.EnemyArena.Reverse
 end
 
 ---Returns true if the left frame is "earlier" than the right frame.
