bb4ebb1c3c345a37464fda1468c70823510f21ae
Verubato <>
2024-05-23 18:29:35 +1000

    #36 - Fixed fast targeting issue.

--- src/Modules/Targeting.lua
+++ src/Modules/Targeting.lua
@@ -356,6 +356,7 @@ end
 function M:Init()
     local targetFriendlyCount = 5
     local targetEnemyCount = 3
+    local downOrUp = wow.GetCVarBool("ActionButtonUseKeyDown") and "AnyDown" or "AnyUp"
 
     if #targetFrames > 0 then
         targetFrames = {}
@@ -367,17 +368,13 @@ function M:Init()
 
     for i = 1, targetFriendlyCount do
         local button = wow.CreateFrame("Button", "FSTarget" .. i, wow.UIParent, "SecureActionButtonTemplate")
-        -- If the ActionButtonUseKeyDown cvar is set to 0, then button down triggers don't work
-        -- seems to be a Blizzard bug since Dragonflight:
-        -- https://us.forums.blizzard.com/en/wow/t/dragonflight-click-bindings-broken/1361972/8
-        -- Adding "AnyUp" fixes it
-        button:RegisterForClicks("AnyDown", "AnyUp")
+        button:RegisterForClicks(downOrUp)
         button:SetAttribute("type", "target")
         button:SetAttribute("unit", "none")
 
         -- pet
         local pet = wow.CreateFrame("Button", "FSTargetPet" .. i, wow.UIParent, "SecureActionButtonTemplate")
-        pet:RegisterForClicks("AnyDown", "AnyUp")
+        button:RegisterForClicks(downOrUp)
         pet:SetAttribute("type", "target")
         pet:SetAttribute("unit", "none")
 
@@ -387,17 +384,17 @@ function M:Init()
 
     for i = 1, targetEnemyCount do
         local target = wow.CreateFrame("Button", "FSTargetEnemy" .. i, wow.UIParent, "SecureActionButtonTemplate")
-        target:RegisterForClicks("AnyDown", "AnyUp")
+        target:RegisterForClicks(downOrUp)
         target:SetAttribute("type", "target")
         target:SetAttribute("unit", "none")
 
         local focus = wow.CreateFrame("Button", "FSFocusEnemy" .. i, wow.UIParent, "SecureActionButtonTemplate")
-        focus:RegisterForClicks("AnyDown", "AnyUp")
+        target:RegisterForClicks(downOrUp)
         focus:SetAttribute("type", "focus")
         focus:SetAttribute("unit", "none")
 
         local pet = wow.CreateFrame("Button", "FSTargetEnemyPet" .. i, wow.UIParent, "SecureActionButtonTemplate")
-        pet:RegisterForClicks("AnyDown", "AnyUp")
+        target:RegisterForClicks(downOrUp)
         pet:SetAttribute("type", "target")
         pet:SetAttribute("unit", "none")
 
@@ -408,7 +405,7 @@ function M:Init()
 
     -- target bottom
     targetBottomFrame = wow.CreateFrame("Button", "FSTargetBottom", wow.UIParent, "SecureActionButtonTemplate")
-    targetBottomFrame:RegisterForClicks("AnyDown", "AnyUp")
+    targetBottomFrame:RegisterForClicks(downOrUp)
     targetBottomFrame:SetAttribute("type", "target")
     targetBottomFrame:SetAttribute("unit", "none")
 
