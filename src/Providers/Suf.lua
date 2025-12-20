d37783c53e391f78f3afc1d3d14b8461dae848a1
Verubato <118912121+Verubato@users.noreply.github.com>
2025-12-20 19:23:59 +1100

    Better namelist container hooks.

--- src/Providers/Suf.lua
+++ src/Providers/Suf.lua
@@ -23,6 +23,15 @@ local function OnEvent(_, event)
     RequestSort(event)
 end
 
+local function EventsFallback()
+    eventFrame = wow.CreateFrame("Frame")
+    eventFrame:HookScript("OnEvent", OnEvent)
+    eventFrame:RegisterEvent(events.GROUP_ROSTER_UPDATE)
+    eventFrame:RegisterEvent(events.PLAYER_ROLES_ASSIGNED)
+    eventFrame:RegisterEvent(events.PLAYER_SPECIALIZATION_CHANGED)
+    eventFrame:RegisterEvent(events.UNIT_PET)
+end
+
 function M:Name()
     return "Shadowed Unit Frames"
 end
@@ -36,12 +45,37 @@ function M:Init()
         return
     end
 
-    eventFrame = wow.CreateFrame("Frame")
-    eventFrame:HookScript("OnEvent", OnEvent)
-    eventFrame:RegisterEvent(events.GROUP_ROSTER_UPDATE)
-    eventFrame:RegisterEvent(events.PLAYER_ROLES_ASSIGNED)
-    eventFrame:RegisterEvent(events.PLAYER_SPECIALIZATION_CHANGED)
-    eventFrame:RegisterEvent(events.UNIT_PET)
+    local canHook = true
+
+    if not ShadowUF then
+        fsLog:Bug("Missing ShadowUF.")
+        canHook = false
+    end
+
+    if ShadowUF and not ShadowUF.OnInitialize then
+        fsLog:Bug("Missing  ShadowUF:OnInitialize.")
+        canHook = false
+    end
+
+    if canHook then
+        wow.hooksecurefunc(ShadowUF, "OnInitialize", function()
+            -- user may have disabled party/arena frames, so don't log if they are missing
+            -- suf also doesn't load arena header unless inside arena
+            if SUFHeaderparty then
+                SUFHeaderparty:HookScript("OnEvent", OnEvent)
+            end
+
+            if SUFHeaderarena then
+                SUFHeaderarena:HookScript("OnEvent", OnEvent)
+            end
+
+            if not SUFHeaderparty and not SUFHeaderarena then
+                EventsFallback()
+            end
+        end)
+    else
+        EventsFallback()
+    end
 end
 
 function M:RegisterRequestSortCallback(callback)
