3382b4ba112a0e30ce57faa70bfc930921f0bd36
Verubato <>
2026-01-05 20:51:44 +1100

    Removed build folder in preparation of adding the devops scripts.

--- build/libs.ps1
+++
@@ -1,2 +0,0 @@
-svn export https://repos.curseforge.com/wow/libuidropdownmenu/trunk/LibUIDropDownMenu ..\src\Libs\LibUIDropDownMenu
-svn export https://repos.curseforge.com/wow/libstub/trunk ..\src\Libs\LibStub
